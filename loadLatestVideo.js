68442849379e8652dfdc6482371063ba22e745dc
Donutask <51319000+Donutask@users.noreply.github.com>
2024-08-14 10:05:24 +1000

    Change front page games, css adjustments, turn off video URL parameter

--- loadLatestVideo.js
+++ loadLatestVideo.js
@@ -2,42 +2,50 @@
 //https://stackoverflow.com/questions/18267426/html-auto-embedding-recent-uploaded-videos-from-a-youtube-channel/45342740#45342740
 //https://stackoverflow.com/questions/35294633/what-is-the-vanilla-js-version-of-jquerys-getjson
 
-var channelID = "UC0OBisfM_ZRwf0sfDJ1Q0YQ";
-var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+// Inserts latest YouTube video to page.
 
-let url = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID
-var request = new XMLHttpRequest();
+const channelID = "UC0OBisfM_ZRwf0sfDJ1Q0YQ";
+const reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+
+const url = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID
+let request = new XMLHttpRequest();
 request.open('GET', url, true);
 
 request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
         // Success!
-        var data = JSON.parse(request.responseText);
+        const data = JSON.parse(request.responseText);
 
-        var link = data.items[0].link;
-        var id = link.substr(link.indexOf("=") + 1);
+        const link = data.items[0].link;
+        const id = link.substr(link.indexOf("=") + 1);
 
-        let vidPlayer = document.createElement("iframe");
+        const vidPlayer = document.createElement("iframe");
         document.getElementById("vidContainer").appendChild(vidPlayer);
         vidPlayer.width = 600;
         vidPlayer.height = 340;
         vidPlayer.frameBorder = 0;
         vidPlayer.allowFullscreen = true;
 
-        let url = "https://youtube.com/embed/" + id + "?controls=0&showinfo=0&rel=0";
+        const url = "https://youtube.com/embed/" + id + "?controls=0&showinfo=0&rel=0";
         vidPlayer.setAttribute("src", url);
         vidPlayer.id = "videoPlayer"
 
         vidPlayer.title = "Latest YouTube video"
     } else {
-        // We reached our target server, but it returned an error
+        // We reached our target server, but it returned an error, ignore
     }
 };
 
 request.onerror = function () {
-    // There was a connection error of some sort
+    // There was a connection error of some sort, ignore it
 };
 
 document.onload = new function () {
-    request.send();
+    //Allow you to add ?hidevideo to not have youtube be annoying
+    const urlParams = new URLSearchParams(window.location.search);
+    const hideVid = urlParams.has('hidevideo');
+
+    if (!hideVid) {
+        request.send();
+    }
 }
